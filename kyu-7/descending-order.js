d88ae9f124377727fe0c123afdbfe47fecadd787
Lucas <lucasdelvillar14@gmail.com>
2022-05-31 16:37:57 -0700

    add new 7kyu

--- kyu-7/descending-order.js
+++ kyu-7/descending-order.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 
 function descendingOrder(n){
-  return Number(String(n).split('').sort((a,b) => b - a).join(''))
+  return Number(n.toString().split('').sort((a,b) => b - a).join(''))
 }
